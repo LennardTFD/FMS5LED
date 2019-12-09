c7760816329b828f1a273afb4b6d57d9b75006b6
Lennard Giesing <lennard.giesing@gmail.com>
2019-12-09 18:41:06 +0100

    _

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var PINS = require("../Constants");
-//var Gpio = require('onoff').Gpio; //require onoff to control GPIO
+var Gpio = require('onoff').Gpio; //require onoff to control GPIO
 var router = express.Router();
 
 
-//var LEDPins = {RED: new Gpio(PINS.RED, 'out'), GREEN: new Gpio(PINS.GREEN, 'out'), BLUE: new Gpio(PINS.BLUE, 'out'), POWER: new Gpio(PINS.POWER, 'out')};
+var LEDPins = {RED: new Gpio(PINS.RED, 'out'), GREEN: new Gpio(PINS.GREEN, 'out'), BLUE: new Gpio(PINS.BLUE, 'out'), POWER: new Gpio(PINS.POWER, 'out')};
 var LEDPreset = PINS.PRESET;
 
 
@@ -67,7 +67,7 @@ router.post("/on", function (req, res, next) {
 
 router.post("/off", function (req, res, next) {
     console.log("LEDs are off");
-    timeout = (new Date()).getTime();;
+    timeout = (new Date()).getTime();
     //switchOff();
     res.status(200);
     res.send();
